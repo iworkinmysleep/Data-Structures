5d3fb521a69733e9d72ff203da42877531158ce6
Jason Hinton <jason_hinton@att.net>
2020-09-16 23:03:59 -0400

    stack with linked list

--- stack/stack.py
+++ stack/stack.py
@@ -29,3 +29,31 @@ class Stack:
             popped = self.storage.pop()
             self.size -= 1
             return popped
+
+
+# With linked list storage
+import sys
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
+
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        else:
+            popped = self.storage.remove_tail()
+            self.size -= 1
+            return popped
