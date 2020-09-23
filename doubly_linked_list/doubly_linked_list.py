99830cc0c1976dc70d5d124b9b93ad6bdd2ac0b0
Jason Hinton <jason_hinton@att.net>
2020-09-22 22:26:45 -0400

    mvp

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,12 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def delete(self): 
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +33,15 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        newListNode = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = newListNode
+            self.tail = newListNode
+        else:
+            newListNode.next = self.head
+            self.head.prev = newListNode
+            self.head = newListNode
         
     """
     Removes the List's current head node, making the
@@ -35,7 +49,9 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+            value = self.head.value
+            self.delete(self.head)
+            return value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,40 +59,90 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
-            
+        newListNode = ListNode(value, None, None)
+        self.length += 1
+        if not self.tail and not self.head:
+            self.tail = newListNode
+            self.head = newListNode
+        else:
+            newListNode.prev = self.tail
+            self.tail.next = newListNode
+            self.tail = newListNode
     """
     Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        value = node.value
+        if node is self.tail:
+            self.remove_from_tail()
+        else:
+            node.delete()
+
+            self.length -= 1
+        self.add_to_head(value)
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        value = node.value
+        if node is self.head:
+            self.remove_from_head()
+        else:
+            node.delete()
+
+            self.length -= 1
+        self.add_to_tail(value)
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        if not self.head and not self.tail:
+            return
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head is node:
+            self.head = node.next
+            node.delete()
+
+        elif self.tail is node:
+            self.tail = node.prev
+            node.delete()
+            
+        else:
+            node.delete()
+        self.length -= 1  
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+        max = self.head.value
+        current = self.head
+        while current:
+            if current.value > max:
+                max = current.value
+            current = current.next
+        return max
